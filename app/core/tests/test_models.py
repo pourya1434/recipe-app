717cc1909b728f8a9a1c992ce34be4f89e680682
pourya1434 <pourya.lotfy@outlook.com>
2023-11-05 01:05:04 +0330

    normilaze email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,20 @@ class ModelTest(TestCase):
     def test_create_user_with_email_successful(self):
         email = "test@example.com"
         password = "testpassword123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )  # create user --> UserManager --> default method
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
