acf7b51307907b5b2639e58e4b6662f5b50f9eca
pourya1434 <pourya.lotfy@outlook.com>
2023-11-06 02:08:28 +0330

    test for private user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,6 +11,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
+ME_URL = reverse("user:me")
 
 
 def create_user(**params):
@@ -108,3 +109,45 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrive_unauthorized_user(self):
+        """Test Authentication is required for user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTest(TestCase):
+    """Test api request that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            name="Test Name", email="test@example.com", password="password123"
+        )
+        self.client = APIClient
+        self.client.force_authenticate(user=self.user)
+
+    def test_retive_profile_success(self):
+        """Test retriveing profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data, {"email": self.user.email, "name": self.user.name}
+        )  # too long
+
+    def test_post_me_not_allowed(self):
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating user profile"""
+        payload = {"name": "updated name", "password": "newPassword123"}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
