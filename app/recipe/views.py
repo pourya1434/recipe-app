fa05b9e1b82f62aa531dbd22e9e4ce7c05138b3d
pourya1434 <pourya.lotfy@outlook.com>
2023-11-11 22:27:15 +0330

    add filter test

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,12 +1,99 @@
-from rest_framework import viewsets, mixins, status
+"""
+Views for the recipe APIs
+"""
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
+from rest_framework import (
+    viewsets,
+    mixins,
+    status,
+)
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.decorators import action
+
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient,
+)
+from recipe import serializers
 
 
-from core.models import Recipe, Tag, Ingredient
-from . import serializers
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "tags",
+                OpenApiTypes.STR,
+                description="Comma separated list of tag IDs to filter",
+            ),
+            OpenApiParameter(
+                "ingredients",
+                OpenApiTypes.STR,
+                description="Comma separated list of ingredient IDs to filter",
+            ),
+        ]
+    )
+)
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return (
+            queryset.filter(user=self.request.user).order_by("-id").distinct()
+        )  # Too long
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+        elif self.action == "upload_image":
+            return serializers.RecipeImageSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(
@@ -15,53 +102,25 @@ class BaseRecipeAttrViewSet(
     mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
-    """Base Recipe attributes viewSet"""
+    """Base viewset for recipe attributes."""
 
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Filter queryset to authenticated user"""
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by("-name")
 
 
-class RecipeViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.RecipeDetailSerializer
-    queryset = Recipe.objects.all()
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by("-id")
-
-    def get_serializer_class(self):
-        if self.action == "list":
-            return serializers.RecipeSerializer
-        elif self.action == "upload_image":
-            return serializers.RecipeImageSerializer
-        return self.serializer_class
-
-    def perform_create(self, serializer):
-        # create a new recipe
-        serializer.save(user=self.request.user)
-
-    @action(methods=["POST"], detail=True, url_path="upload-image")
-    def upload_image(self, request, pk=None):
-        """Upload an image to recipe"""
-        recipe = self.get_object()
-        serializers = self.get_serializer(recipe, data=request.data)
-
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_200_OK)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database."""
+
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
 
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database."""
+
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
